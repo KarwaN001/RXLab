4b95d141f7b613329ca4924b8704b8c169739a34
KarwaN001 <karwanusf1@gmail.com>
2025-09-16 12:38:48 -0700

    Initial commit

---
+++ RXLab/RXLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  RXLabApp.swift
+//  RXLab
+//
+//  Created by karwan Syborg on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RXLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
