45c83ee63675f7489454a72d2d8aa674b7467e72
KarwaN001 <karwanusf1@gmail.com>
2025-09-16 16:07:44 -0700

    Add ObservableViewController and ObservableViewModel with UI components and RxSwift bindings for observable value emission

--- RXLab/Screens/Observable/ObservableViewController.swift
+++ RXLab/Screens/Observable/ObservableViewController.swift
@@ -5,4 +5,93 @@
 //  Created by karwan Syborg on 16/09/2025.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
+import RxSwift
+import RxCocoa
+
+class ObservableViewController: UIViewController {
+
+    private let viewModel = ObservableViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private let label: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Press button to see Observable"
+        lbl.textAlignment = .center
+        lbl.font = .systemFont(ofSize: 18)
+        lbl.numberOfLines = 0
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+    
+    private let button: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Emit Values", for: .normal)
+        btn.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.2)
+        btn.layer.cornerRadius = 8
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Observable Example"
+        
+        setupViews()
+        bindViewModel()
+    }
+    
+    private func setupViews() {
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 30),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: 150),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    private func bindViewModel() {
+        // Bind items emitted from viewModel to label
+        viewModel.items
+            .scan("") { previous, new in
+                previous + "\n" + new
+            }
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    @objc private func buttonTapped() {
+        viewModel.fetchItems()
+    }
+}
+
+
+// MARK: - SwiftUI Preview
+
+struct ObservableViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ObservableViewController {
+        return ObservableViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ObservableViewController, context: Context) {
+        
+    }
+}
+
+struct ObservableViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ObservableViewControllerRepresentable()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
--- RXLab/Screens/Observable/ObservableViewModel.swift
+++ RXLab/Screens/Observable/ObservableViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by karwan Syborg on 16/09/2025.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+class ObservableViewModel {
+
+    // Dispose bag for memory management
+    let disposeBag = DisposeBag()
+    
+    // Output: Observable of strings
+    let items = PublishSubject<String>()
+    
+    // Function to simulate emitting values
+    func fetchItems() {
+        let numbers = Observable.of("One", "Two", "Three")
+        
+        numbers
+            .subscribe(onNext: { [weak self] value in
+                self?.items.onNext(value)
+            })
+            .disposed(by: disposeBag)
+    }
+}
